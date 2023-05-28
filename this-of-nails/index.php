027c7753686b60524b8547f7660020a1862dc46e
SusanaSamaniego <53665020+SusanaSamaniego@users.noreply.github.com>
2023-05-28 11:18:35 +0200

    Add files via upload

---
+++ this-of-nails/index.php
@@ -0,0 +1,11 @@
+<?php
+
+<script>href='index.html';</script>
+
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!";
+require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
+?>
+
+
